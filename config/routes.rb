d465d905bf6d432f30d34311be097ea4de3c01ba
Lucie Oken <lucie.oken@kellogg.northwestern.edu>
2024-06-04 22:02:33 +0000

    Corrected application view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "places", :action => "index" })
-  resources "entries"
-  resources "places"
-  resources "sessions"
-  resources "users"
+  root 'places#index'
+
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :places
+  resources :entries
+
+  get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  delete 'logout', to: 'sessions#destroy'
 end
